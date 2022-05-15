2903cb2d6ada0f718fe3c7c315aeed440c7e0dc9
jae woo han <40654123+jae-woo-han@users.noreply.github.com>
2022-05-16 07:28:45 +0900

    setting allowedMethod(GET,POST,PUT,DELETE)

--- src/main/java/com/study/board/config/WebConfig.java
+++ src/main/java/com/study/board/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.study.board.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
@@ -9,7 +10,13 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
 
-        registry.addMapping("/api/**");
+        registry.addMapping("/api/**")
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name()
+                );
         registry.addMapping("/file/**");
     }
 }
